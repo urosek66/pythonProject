ae6a06bdc47b1ef6bec82b11efa214c95375f680
Uroš Rojnik <97902369+urosek66@users.noreply.github.com>
2022-07-16 14:57:53 +0200

    Adding some new files
    New homework projects.

---
+++ StringFunctions/main.py
@@ -0,0 +1,9 @@
+# how to make string to lowercase
+
+weather = "Today Is A BeautiFul DAY"
+print(weather.lower())
+
+# how to make string to uppercase
+
+a = "Hello World"
+print(a.upper())
---
+++ UnitConverter/main.py
@@ -0,0 +1,12 @@
+print("Hello this app is a simple unit converter.Here you can convert km in miles")
+
+try_again = None
+
+while try_again == "Yes" or "yes" or "YES":
+    kilometer = int(input("Enter number of kilometers: "))
+    miles = kilometer / 1.609344
+    print(f"{kilometer} km is {miles} miles")
+
+    try_again = (input("Do you want to continue(Type Yes, yes or YES): "))
+
+print("Goodbye and see you soon")
