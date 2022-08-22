3f78db1d7359d9bf805607374d54a10f4aa13702
Uroš Rojnik <97902369+urosek66@users.noreply.github.com>
2022-08-22 16:42:34 +0200

    Update main.py
    Some changes to the code

--- Geography_Quiz/main.py
+++ Geography_Quiz/main.py
@@ -1,10 +1,10 @@
 import random
 
-capital_cities = {"Slovenia": "Ljubljana", "Serbia": "Belgrade",
-  "Croatia": "Zagreb", "Germany": "Berlin",
-  "Netherlands":"Amsterdam", "Belgium": "Brussels",
-  "Hungary": "Budapest", "Ireland":  "Dublin",
-   "Finland":  "Helsinki", "Spain":  "Madrid"}
+capital_cities = {"Slovenia": "ljubljana", "Serbia": "belgrade",
+                  "Croatia": "zagreb", "Germany": "berlin",
+                  "Netherlands": "amsterdam", "Belgium": "brussels",
+                  "Hungary": "budapest", "Ireland":  "dublin",
+                  "Finland":  "helsinki", "Spain":  "madrid"}
 
 print("Hello this is a simple geography quiz")
 
@@ -14,8 +14,9 @@ check = capital_cities.get(country)
 while True:
     print("What is the capital city of " + country + " ? ")
     answer = input("")
+    answer_1 = answer.lower()
 
-    if answer == check:
+    if answer_1 == check:
         print("Congratulations you are correct")
         break
     else:
